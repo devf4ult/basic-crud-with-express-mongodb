5baba7d4c3a59ea9b39ebe9aa6ef3272af3289b7
runtime3rr0r <devf4ult@gmail.com>
2021-10-25 14:29:07 +0700

    Basic CRRUD Operation with Express & Mongodb

---
+++ public/main.js
@@ -0,0 +1,45 @@
+
+const update = document.querySelector('#update-button')
+const deleteButton = document.querySelector('#delete-button')
+const messageDiv = document.querySelector('#message')
+
+update.addEventListener('click', _ => {
+    fetch('/quotes', {
+      method: 'put',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        name: 'Darth Vadar',
+        quote: 'I find your lack of faith disturbing.'
+      })
+    })
+  })
+
+
+deleteButton.addEventListener('click', _ => {
+  fetch('/quotes', {
+    method: 'delete',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      name: 'Darth Vadar'
+    })
+  })
+    .then(res => {
+      if (res.ok) return res.json()
+    })
+    .then(data => {
+      window.location.reload()
+    })
+})
+
+deleteButton.addEventListener('click', _ => {
+  fetch(/* ... */)
+    .then(/* ... */)
+    .then(response => {
+      if (response === 'No quote to delete') {
+        messageDiv.textContent = 'No Darth Vadar quote to delete'
+      } else {
+        window.location.reload(true)
+      }
+    })
+    .catch(/* ... */)
+})
---
+++ server.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const bodyParser= require('body-parser');
+const app = express();
+const MongoClient = require('mongodb').MongoClient
+const connectionString = "mongodb+srv://devfault:k1ll1t0r0us@cluster0.wsehm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static('public'))
+app.use(bodyParser.json())
+
+
+MongoClient.connect(connectionString, { useUnifiedTopology: true })
+  .then(client => {
+    console.log('Connected to Database')
+    const db = client.db('star-wars-quotes')
+    const quotesCollection = db.collection('quotes')
+    
+    app.get('/', (req, res) => {
+      db.collection('quotes').find().toArray()
+        .then(results => {
+          console.log(results)
+          res.render('index.ejs', { quotes: results })
+        })
+        .catch('')
+    })
+
+    app.post('/quotes', (req, res) => {
+      quotesCollection.insertOne(req.body)
+        .then(result => {
+          res.redirect('/')
+        })
+        .catch(error => console.error(error))
+    })
+
+    app.put('/quotes', (req, res) => {
+      console.log(req.body)
+      quotesCollection.findOneAndUpdate(
+        { name: 'rafli' },
+        {
+          $set: {
+            name: req.body.name,
+            quote: req.body.quote
+          }
+        },
+        {
+          upsert: true
+        }
+      )
+        .then(result => {
+          console.log(result)
+         }) 
+        .catch(error => console.error(error))
+    })
+
+    app.delete('/quotes', (req, res) => {
+      quotesCollection.deleteOne( { name: req.body.name })
+        .then(result => {
+          if (result.deletedCount === 0) {
+            return res.json('No quote to delete')
+          }
+          res.json(`Deleted Darth Vadar's quote`)
+        })
+        .catch(error => console.error(error))
+    })
+    
+
+  })
+  .catch(console.error)
+
+
+app.listen(3000, function() {
+  console.log('listening on 3000')
+})
